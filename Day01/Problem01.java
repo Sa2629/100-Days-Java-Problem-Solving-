4683929d5c9692df45bcfa6ba796226fdee4e2f8
Santosh <gajjalasantosh307@gmail.com>
2026-09-25 20:22:32 +0000

    just added comments

--- Day01/Problem01.java
+++ Day01/Problem01.java
@@ -3,6 +3,7 @@ package Javachallenge100days.Day01;
 public class Problem01 {
     public static void main(String[] args) {
         // find count of even odd from an array
+        // bruteforce approach will update later to an optimized way
         int[] arr = {12,200,39,1,3,9,56};
         int even = 0;
         int odd = 0;
@@ -19,3 +20,6 @@ public class Problem01 {
         System.out.println("odd numbers are: " + odd);
     }
 }
+
+// time complexity = O(N)
+// space complexity = O(1)
---
+++ Day02/Problem02.java
@@ -0,0 +1,19 @@
+package Javachallenge100days.Day02;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        // Mathematics => Basics 
+        // Sum of Natural Numbers
+        // brute force solution will add optimized approach soon
+        int sum = 0;
+        for(int i = 1; i <= 50; i++){
+            sum += i;
+        } 
+        System.out.println("the sum of n natural numbers from 1 to 50 is: " + sum);
+    }
+}
+
+
+// space complexity is O(1)
+// time complexity is O(1)
+// 
